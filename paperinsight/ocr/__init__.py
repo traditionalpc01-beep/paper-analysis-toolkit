11e6cc65a325deb7e9cb4d040e175c9ca242f060
yaozhuoyi <traditionalpc1@outlook.com>
2026-03-16 09:37:41 +0800

    refactor: 重构OCR模块，移除百度API，新增PaddleX API

--- paperinsight/ocr/__init__.py
+++ paperinsight/ocr/__init__.py
@@ -1,7 +1,7 @@
 """OCR 模块"""
 
 from paperinsight.ocr.base import BaseOCR
-from paperinsight.ocr.baidu_api import BaiduOCRAPI
 from paperinsight.ocr.local import LocalOCR
+from paperinsight.ocr.paddlex_api import PaddleXAPI
 
-__all__ = ["BaseOCR", "BaiduOCRAPI", "LocalOCR"]
+__all__ = ["BaseOCR", "LocalOCR", "PaddleXAPI"]
