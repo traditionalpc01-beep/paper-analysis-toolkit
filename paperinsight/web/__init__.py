48a689c4f434273db2d3f9fbbc075e83e95ce92c
yaozhuoyi <poker0109@outlook.com>
2026-03-17 12:07:10 +0800

    feat: add MJL journal resolver

--- paperinsight/web/__init__.py
+++ paperinsight/web/__init__.py
@@ -1,5 +1,6 @@
 """Web 搜索模块"""
 
 from paperinsight.web.impact_factor_search import ImpactFactorSearcher
+from paperinsight.web.journal_resolver import MJLJournalResolver
 
-__all__ = ["ImpactFactorSearcher"]
+__all__ = ["ImpactFactorSearcher", "MJLJournalResolver"]
